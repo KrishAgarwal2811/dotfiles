1049546fc6ce8d57ca1c87ccd9fbd7de5beb3f2b
Krish <68506145+KrishAgarwal2811@users.noreply.github.com>
2022-07-09 20:51:36 +0530

    add: install script for codespaces

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Install and configure Starship
+curl -sS https://starship.rs/install.sh | sh
+echo "eval \"\$(starship init zsh)\"" >> ${ZSOTDIR:-$HOME}/.zshrc
+
+# Install and configure ZSH Auto Suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZSOTDIR:-$HOME}/.zshrc
+
+# Install and configure ZSH Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+# Remove the `Systemd` message in starship terminal
+mkdir -p ~/.config && printf "[container]\ndisabled = true" >> ~/.config/starship.toml
